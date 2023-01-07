1ededda07f9d8e424c0c03fb30b4b48ce9b612a6
Thanu <thanuja99shetty@gmail.com>
2023-01-07 03:52:08 +0000

    new job modification

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -20,7 +20,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from Main Method & Good Afternoon from Thanushetty to all!!!");
+	  System.out.println("Hello from Main Method and Good morning  to all!!!");
 	  
   }
 	
